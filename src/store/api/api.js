da92d0dbdf92000eea3c3a353c200f8f1d3ab09a
Yahampath Chandika <yhmpth@gmail.com>
2024-09-25 12:45:15 +0530

    saddsad

--- src/store/api/api.js
+++ src/store/api/api.js
@@ -3,8 +3,8 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 // Initialize an empty api service that we'll inject endpoints into later as needed
 export const api = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:4000/',
-        // baseUrl: 'http://44.204.115.155:4000/',
+        // baseUrl: 'http://localhost:4000/',
+        baseUrl: 'http://44.204.115.155:4000/',
         prepareHeaders: (headers) => {
             const accessToken = localStorage.getItem('accessToken'); 
             // console.log("hello "+accessToken)         
