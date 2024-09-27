d23f1c2a2a9e8c812d0078f51ab511dd40d53152
Yahampath Chandika <yhmpth@gmail.com>
2024-09-27 19:22:13 +0530

    ngrokimage

--- src/components/common/UserDetails.jsx
+++ src/components/common/UserDetails.jsx
@@ -4,24 +4,12 @@ import { useGetSignedUserQuery } from "../../store/api/userApi";
 export default function UserDetails() {
   const { data: signedUser } = useGetSignedUserQuery();
   const user = signedUser?.payload;
-
-  // Determine if the app is running locally or on hosted environment
-  const isLocalhost = window.location.hostname === "localhost";
-  
-  // Determine the base URL based on the environment
-  const baseUrl = isLocalhost
-    ? "http://localhost:4000"
-    : "http://44.204.115.155:4000"; // Replace with your hosted backend URL
-  
-  // Convert image path based on the OS environment
-  const imagePath = isLocalhost 
-    ? user?.image.replace(/\//g, "\\") // Convert to Windows-style backslashes locally
-    : user?.image.replace(/\\/g, "/"); // Convert to Linux-style forward slashes for hosting
-  
+  console.log("User", user)
   return (
     <div className="flex">
       <img
-        src={`${baseUrl}/${imagePath}`} // Use the modified image path
+        src={`http://localhost:4000/${user?.image}`}
+        // src={`http://44.204.115.155:4000/${user?.image}`}
         alt="Profile"
         className="w-12 h-12 rounded-full mr-5"
       />
